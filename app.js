3f15b1ccd77a1daf10f2c43fdd173c92b870c671
MarrocchiProgrammer <tomasmarrocchi@gmail.com>
2023-05-25 19:35:23 -0300

    25-5

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-// const open = document.getElementById('open');
-// const modal_container = document.getElementById('modal_container');
-// const close = document.getElementById('close');
+function toggleCardButtonColors() {
+    const card = document.querySelector('.card');
+    const button = document.querySelector('.boton-verFotos');
 
-// open.addEventListener('click', () => {
-//   modal_container.classList.add('show');  
-// });
-
-// close.addEventListener('click', () => {
-//   modal_container.classList.remove('show');
-// });
+    card.classList.toggle('card-hover');
+    button.classList.toggle('boton-hover');
+}
